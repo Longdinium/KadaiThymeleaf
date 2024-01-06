98b432ee2198acd20a58e82b83a8403f58a18858
Longdinium <rondobell0303@gmail.com>
2024-01-06 22:36:44 +0900

    output.htmlを修正

--- src/main/java/com/techacademy/OutputController.java
+++ src/main/java/com/techacademy/OutputController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class OutputController {
 
     @PostMapping("/output")
-    public String postOutput(@RequestParam("val") String val, Model model) {
-        // input.htmlのフォームから送信されてきた値（val）をModelに登録
-        model.addAttribute("val", val);
+    public String postOutput(@RequestParam("previous") String val, Model model) {
+        // input.htmlのフォームから送信されてきた値（「val」という名前）を「previous」という名前でModelに登録
+        model.addAttribute("previous", val);
         // output.htmlに画面遷移
         return "output";
     }
